f886642f63069592e5b1cc6379d1b4f61626c510
jadonamite <jadonamite@gmail.com>
2026-05-05 02:03:17 +0100

    refactor: wip (quick fix)

--- src/components/fundx/hero/index.tsx
+++ src/components/fundx/hero/index.tsx
@@ -1,10 +1,10 @@
 "use client"
 
-import { HeroBadge } from "./HeroBadge"
-import { HeroHeadline } from "./HeroHeadline"
 import { HeroCTAs } from "./HeroCTAs"
 import { HeroDeckSlot } from "./HeroDeckSlot"
 import HeroLogoParallax from "./HeroBackdrop"
+import { HeroBadge } from "./HeroBadge"
+import { HeroHeadline } from "./HeroHeadline"
 
 
 export { HeroDeckSlot }
