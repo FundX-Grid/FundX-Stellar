7806b673738d1137d0c967cfb7e685d24eb4be3a
jadonamite <jadonamite@gmail.com>
2026-05-05 11:39:33 +0100

    fix: correct ui handling (progress)

--- src/components/fundx/Providers.tsx
+++ src/components/fundx/Providers.tsx
@@ -1,16 +1,16 @@
 "use client"
 
-import "@rainbow-me/rainbowkit/styles.css"
 import { WagmiProvider } from "wagmi"
-import { config } from "@/lib/celo-config"
 import { RainbowKitProvider } from "@rainbow-me/rainbowkit"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { config_ } from "@/lib/celo-config_"
+import "@rainbow-me/rainbowkit/styles.css"
 
 const queryClient = new QueryClient()
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WagmiProvider config={config}>
+    <WagmiProvider config_={config_}>
       <QueryClientProvider client={queryClient}>
         <RainbowKitProvider>
           {children}
